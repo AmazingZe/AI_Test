8ae0c817b192a04cb5e24f79a33c2d0b8181f489
AmazingZe <515102101@qq.com>
2019-06-24 00:07:20 +0800

    no message

---
+++ AI_Test/Assets/Scripts/GameUtils/Pool/Pool.cs
@@ -0,0 +1,74 @@
+﻿namespace GameUtils
+{
+    public class Pool<T> : Singleton<Pool<T>> where T : class, IPoolable, new()
+    {
+        private const int InitSize = 8;
+
+        #region Properties
+        private int m_FirstFree = 0;
+
+        private T[] m_Pools;
+        #endregion
+
+        #region Public_API
+        public static T Allocate()
+        {
+            return Instance._Allocate();
+        }
+        public static void Recycle(ref T item)
+        {
+            if (ReferenceEquals(item, null)) return;
+
+            Instance._Recycle(item);
+
+            item = null;
+        }
+        #endregion
+
+        #region ISingleton_API
+        private Pool() { }
+
+        public override void OnInit()
+        {
+            m_Pools = new T[InitSize];
+            for (int i = 0; i < InitSize; i++)
+            {
+                m_Pools[i] = new T();
+                m_Pools[i].Recycle();
+            }
+        }
+
+        protected override void _OnRelease()
+        {
+            
+            base._OnRelease();
+        }
+        #endregion
+
+        private T _Allocate()
+        {
+            if (m_FirstFree >= m_Pools.Length)
+            {
+                int oldSize = m_Pools.Length;
+                var newPool = new T[oldSize * 2];
+                int i = 0;
+                for (; i < oldSize; i++)
+                    newPool[i] = m_Pools[i];
+                for (; i < oldSize * 2; i++)
+                {
+                    newPool[i] = new T();
+                    newPool[i].Recycle();
+                }
+
+                m_Pools = newPool;
+            }
+
+            return m_Pools[m_FirstFree++];
+        }
+        private void _Recycle(T item)
+        {
+            item.Recycle();
+            m_Pools[--m_FirstFree] = item;
+        }
+    }
+}
---
+++ AI_Test/Assets/Scripts/GameUtils/Pool/SafePool.cs
@@ -0,0 +1,49 @@
+﻿namespace GameUtils
+{
+    using System.Collections.Generic;
+
+    public class SafePool<T> : Singleton<SafePool<T>> where T : class, IPoolable, new ()
+    {
+        private static object m_Lock = new object();
+
+        private HashSet<T> m_Check;
+
+        #region Public_API
+        public static T Allocate()
+        {
+            lock (m_Lock)
+            {
+                var retMe = Pool<T>.Allocate();
+                Instance.m_Check.Remove(retMe);
+                return retMe;
+            }
+        }
+        public static void Recycle(ref T item)
+        {
+            if (!Instance.m_Check.Add(item)) return;
+
+            Pool<T>.Recycle(ref item);
+        }
+        #endregion
+
+        #region ISingleton_API
+        private SafePool() { }
+
+        public override void OnInit()
+        {
+            lock (m_Lock)
+            {
+                m_Check = new HashSet<T>();
+            }
+        }
+        protected override void _OnRelease()
+        {
+            lock (m_Lock)
+            {
+                Pool<T>.OnRelease();
+                base._OnRelease();
+            }
+        }
+        #endregion
+    }
+}
